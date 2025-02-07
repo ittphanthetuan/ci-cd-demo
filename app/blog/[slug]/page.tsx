b6142bf0d4453e2fb153586e1bcb0bdac377a535
Tuan Phan <ittphanthetuan@gmail.com>
2025-02-07 12:18:17 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,12 +1,11 @@
 type Params = {
-  params: {
-    slug: string;
-  };
+  params: any;
 };
 
 export async function generateMetadata({ params }: Params) {
   return { title: `Post: ${params.slug}` };
 }
+//
 //
 export default async function Page({ params }: Params) {
   const { slug } = await params;
