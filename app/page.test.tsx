507ca5f15240efb7171b75b657a178c8d6760f38
Tuan Phan <ittphanthetuan@gmail.com>
2025-02-07 11:48:23 +0700

    update

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -8,3 +8,8 @@ it("App Router: Works with Server Components", () => {
   render(<Page />);
   expect(screen.getByRole("heading")).toHaveTextContent("App Router");
 });
+
+it("HoleText: Check hole text 2", () => {
+  render(<Page />);
+  expect(screen.getByText("HoleText")).toBeInTheDocument();
+});
